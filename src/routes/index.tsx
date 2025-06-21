fc73dd0419e1cbdf612066fd97afa5adbe7749c1
Astro-67 <ibrahimsa477@gmail.com>
2025-06-21 10:13:55 +0000

    feat: initialize bookissue-frontend with Vite, React, and TanStack Router
    - Add index.html as the main entry point for the application.
    - Create package.json with scripts for development, build, and testing.
    - Include favicon and logo images for the application.
    - Add manifest.json for PWA support.
    - Implement robots.txt for search engine directives.
    - Set up main.tsx to initialize the React application with TanStack Router.
    - Create reportWebVitals.ts for performance measurement.
    - Generate routeTree.gen.ts for routing structure.
    - Define root and index routes in separate files.
    - Add Tailwind CSS for styling in styles.css.
    - Configure TypeScript with tsconfig.json for strict type checking.
    - Set up Vite configuration in vite.config.ts with necessary plugins.

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold ">
+    Hello world!
+  </h1>
+  )
+}
